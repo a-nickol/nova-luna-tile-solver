8cceddaeac965824616abe789ac019fa1ad78a29
anickol <dark5@gmx.de>
2021-09-22 06:23:02 +0200

    reorganized structure

---
+++ src/solver.rs
@@ -0,0 +1,48 @@
+use crate::nova_luna::{Move, State};
+use mcts::transposition_table::ApproxTable;
+use mcts::tree_policy::UCTPolicy;
+use mcts::{CycleBehaviour, Evaluator, SearchHandle, MCTS};
+
+pub struct StateEvaluator;
+
+impl Evaluator<NovaLunaBoardGameMCTS> for StateEvaluator {
+    type StateEvaluation = i64;
+
+    fn evaluate_new_state(
+        &self,
+        _state: &State,
+        _moves: &Vec<Move>,
+        _: Option<SearchHandle<NovaLunaBoardGameMCTS>>,
+    ) -> (Vec<()>, i64) {
+        todo!()
+    }
+
+    fn interpret_evaluation_for_player(&self, evaln: &i64, _player: &()) -> i64 {
+        *evaln
+    }
+
+    fn evaluate_existing_state(
+        &self,
+        _: &State,
+        evaln: &i64,
+        _: SearchHandle<NovaLunaBoardGameMCTS>,
+    ) -> i64 {
+        *evaln
+    }
+}
+
+#[derive(Default)]
+pub struct NovaLunaBoardGameMCTS;
+
+impl MCTS for NovaLunaBoardGameMCTS {
+    type State = State;
+    type Eval = StateEvaluator;
+    type NodeData = ();
+    type ExtraThreadData = ();
+    type TreePolicy = UCTPolicy;
+    type TranspositionTable = ApproxTable<Self>;
+
+    fn cycle_behaviour(&self) -> CycleBehaviour<Self> {
+        CycleBehaviour::UseCurrentEvalWhenCycleDetected
+    }
+}
