dc04af403246dac3963167ed422e82ebbf613f73
anickol <dark5@gmx.de>
2021-09-23 20:21:55 +0200

    instant for execution

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use crate::solver::StateEvaluator;
 use mcts::transposition_table::*;
 use mcts::tree_policy::*;
 use mcts::*;
+use std::time::Instant;
 
 fn main() {
     let unplaced_tiles = vec![
@@ -108,19 +109,27 @@ fn main() {
         ),
     ];
 
+    let start = Instant::now();
+
     let num_tiles = unplaced_tiles.len();
-    let game = State::with_tiles(unplaced_tiles);
+    let game = State::with_tiles(unplaced_tiles.clone());
 
     let mut mcts = MCTSManager::new(
         game,
         NovaLunaBoardGameMCTS,
         StateEvaluator,
-        UCTPolicy::new(0.5),
+        UCTPolicy::new(2.0),
         ApproxTable::new(1024),
     );
 
-    mcts.playout_n_parallel(10000, 4);
+    mcts.playout_n_parallel(100000, 4);
+
+    let mut game = State::with_tiles(unplaced_tiles);
     for m in mcts.principal_variation(num_tiles) {
+        game.make_move(&m);
         eprintln!("{:?}", m);
     }
+    eprintln!("{}", game.count_solved_tasks());
+
+    eprintln!("elapsed: {:?}", start.elapsed());
 }
