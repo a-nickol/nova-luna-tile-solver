bde9c832ae21a070618217eb3230ee0e5a7ec715
a-nickol <dark5@gmx.de>
2021-09-28 21:53:10 +0200

    angular package

--- nova-luna-gui/src/app/app.module.ts
+++ nova-luna-gui/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TileComponent } from './tile/tile.component';
+import { GameBoardComponent } from './game-board/game-board.component';
+import { TaskComponent } from './task/task.component';
+import { TileBgColorPipe } from './tile-bg-color.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TileComponent,
+    GameBoardComponent,
+    TaskComponent,
+    TileBgColorPipe
   ],
   imports: [
     BrowserModule,
