309bf70d32df84175d657986244db57f4b05d814
a-nickol <dark5@gmx.de>
2021-09-28 21:53:55 +0200

    model

---
+++ nova-luna-gui/src/app/shared/played-tile.ts
@@ -0,0 +1,6 @@
+import { Tile } from "./tile";
+
+export interface PlayedTile {
+  position: number[];
+  tile: Tile;
+}
--- nova-luna-gui/src/app/shared/tile.ts
+++ nova-luna-gui/src/app/shared/tile.ts
@@ -1,4 +1,6 @@
+import { Task } from "./task";
 export interface Tile {
     cost: string;
     color: string;
+    tasks: Task[];
 }
